7a7eb756ecc1ab903b54d7f0725352dadb6a69c2
Anirudh Goel <anirudhgoel.delhi@gmail.com>
2017-03-17 10:10:02 +0530

    added project_root and static_root for static file collection in heroku

--- hackerearth/settings.py
+++ hackerearth/settings.py
@@ -117,5 +117,6 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.10/howto/static-files/
-
+PROJECT_ROOT = os.path.dirname(os.path.abspath(__file__))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, 'static')
 STATIC_URL = '/static/'
