c69c92c3a825cf1d18cd253a601adb35fb336b34
Anirudh Goel <anirudhgoel.delhi@gmail.com>
2017-03-19 21:20:15 +0530

    added herokuapp.com to allowed hosts

--- hackerearth/settings.py
+++ hackerearth/settings.py
@@ -31,7 +31,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [".herokuapp.com"]
 
 
 # Application definition
