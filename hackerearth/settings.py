1ba4842cfb372be68129338ed04163f437ba9225
Anirudh Goel <anirudhgoel.delhi@gmail.com>
2017-03-19 17:55:25 +0530

    changed settings.py to change db to postgresql from sqlite

--- hackerearth/settings.py
+++ hackerearth/settings.py
@@ -73,11 +73,17 @@ WSGI_APPLICATION = 'hackerearth.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/1.10/ref/settings/#databases
+# The application is connected to PostgreSQL DB using user/role - hackerearth (password="")
+# and database name = 'hackerearth'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'hackerearth',
+        'USER': 'hackerearth',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'POST': '',
     }
 }
 
